e4ba35d02b585ff4a125eab6b196895101733e69
stevebstorm <steve.lorent@gmail.Com>
2024-12-16 14:17:02 +0100

    Correctif EXO 1

---
+++ BlazorDemoWASM/Pages/Exercices/Exo1.razor.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorDemoWASM.Pages.Exercices
+{
+    public partial class Exo1
+    {
+        public string Prenom { get; set; }
+        public List<string> Reponses { get; set; }
+        public bool PartieTerminee { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Reponses = new List<string>();
+        }
+
+        public void EnregistrerReponse(string reponse)
+        {
+            Reponses.Add(reponse);
+            if(Reponses.Count > 2)
+            {
+                PartieTerminee = true;
+            }
+        }
+    }
+}
---
+++ BlazorDemoWASM/Pages/Exercices/Quizz.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorDemoWASM.Pages.Exercices
+{
+    public partial class Quizz
+    {
+        [Parameter]
+        public string Prenom { get; set; }
+        [Parameter]
+        public EventCallback<string> RepondreEvent { get; set; }
+        public List<string> Questions { get; set; }
+        public int compteur { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Questions = new List<string>();
+            Questions.Add("Avez vous bien mangé à midi ?");
+            Questions.Add("La sieste était bonne ?");
+            Questions.Add("C'est pas l'heure de l'apéro ?");
+
+            compteur = 0;
+        }
+
+        public void Repondre(string reponse)
+        {
+            RepondreEvent.InvokeAsync(reponse);
+            compteur++;
+        }
+    }
+}
