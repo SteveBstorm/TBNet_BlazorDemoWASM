4c65337c18cf2d31c338d2173a3445715dc9a9ad
stevebstorm <steve.lorent@gmail.Com>
2024-12-18 10:20:01 +0100

    AddHttpClient

--- BlazorDemoWASM/Pages/Exercices/MovieManager/MovieList.razor.cs
+++ BlazorDemoWASM/Pages/Exercices/MovieManager/MovieList.razor.cs
@@ -1,4 +1,8 @@
 ﻿using BlazorDemoWASM.Models;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
 
 namespace BlazorDemoWASM.Pages.Exercices.MovieManager
 {
@@ -8,17 +12,22 @@ namespace BlazorDemoWASM.Pages.Exercices.MovieManager
 
         public Movie SelectedMovie { get; set; }
 
-        protected override void OnInitialized()
+        [Inject]
+        public HttpClient Client { get; set; }
+
+        protected async override Task OnInitializedAsync()
         {
             Movies = new List<Movie>();
-            Movies.Add(new Movie
-            {
-                Id = 1, 
-                Title = "Star Wars : New Hope",
-                ReleaseYear = 1977,
-                Realisator = "George Lucas",
-                Synopsis = "Un pirate et un wookie court après la princesse pour ..."
-            });
+            Movies = await Client.GetFromJsonAsync<List<Movie>>("movie");
+            //Movies = new List<Movie>();
+            //Movies.Add(new Movie
+            //{
+            //    Id = 1, 
+            //    Title = "Star Wars : New Hope",
+            //    ReleaseYear = 1977,
+            //    Realisator = "George Lucas",
+            //    Synopsis = "Un pirate et un wookie court après la princesse pour ..."
+            //});
         }
 
         private void Detail(Movie m)
@@ -31,11 +40,15 @@ namespace BlazorDemoWASM.Pages.Exercices.MovieManager
             Movies.Remove(m);
         }
 
-        private void Add(Movie m)
+        private async Task Add(Movie m)
         {
-            int id = Movies.Max(x => x.Id) + 1;
-            m.Id = id;
-            Movies.Add(m);
+            //string json = JsonSerializer.Serialize(m);
+            //HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            await Client.PostAsJsonAsync("movie", m);
+            //int id = Movies.Max(x => x.Id) + 1;
+            //m.Id = id;
+            //Movies.Add(m);
         }
     }
 }
