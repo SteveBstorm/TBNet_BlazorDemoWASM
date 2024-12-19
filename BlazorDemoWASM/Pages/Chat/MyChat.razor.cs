171cb9f2f896bf78dd86546a0042e84081a9fef3
stevebstorm <steve.lorent@gmail.Com>
2024-12-19 12:15:36 +0100

    Add SignalR

---
+++ BlazorDemoWASM/Pages/Chat/MyChat.razor.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace BlazorDemoWASM.Pages.Chat
+{
+    public partial class MyChat
+    {
+        //Microsoft.AspNetCore.SignalR.Client
+        public List<string> MessageList { get; set; }
+
+        public HubConnection Connection { get; set; }
+
+        public string NewMessage { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            MessageList = new List<string>();
+
+            Connection = new HubConnectionBuilder()
+                    .WithUrl("https://localhost:7010/chathub").Build();
+
+            await Connection.StartAsync();
+
+            Connection.On("notifyNewMessage", (string message) =>
+            {
+                MessageList.Add(message);
+                StateHasChanged();
+            });
+        }
+
+        public async Task SendNewMessage()
+        {
+            await Connection.SendAsync("SendMessage", NewMessage);
+        }
+
+        public async Task JoinGroup()
+        {
+            await Connection.SendAsync("JoinGroup", "blazorgroup");
+            Connection.On("notifyTo_blazorgroup", (string message) =>
+            {
+                MessageList.Add(message);
+                StateHasChanged();
+            });
+        }
+
+        public async Task SendToGroup()
+        {
+            await Connection.SendAsync("SendToGroup", "blazorgroup", NewMessage);
+        }
+    }
+}
