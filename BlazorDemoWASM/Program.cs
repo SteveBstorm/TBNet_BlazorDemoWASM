b41a8c7752db5c5a9f0be1f1068292dfd5d38a84
stevebstorm <steve.lorent@gmail.Com>
2024-12-19 10:44:36 +0100

    I18N demo

--- BlazorDemoWASM/Program.cs
+++ BlazorDemoWASM/Program.cs
@@ -1,8 +1,11 @@
 using BlazorDemoWASM;
 using BlazorDemoWASM.Pages.Auth;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -44,4 +47,45 @@ builder.Services.AddSingleton<AuthenticationStateProvider, MyAuthStateProvider>(
     => Permet de cacher/afficher une partie de la vue/composant en fonction de l'état de connexion
  */
 
-await builder.Build().RunAsync();
+//System.Globalization
+//Microsoft.AspNetCore.Localization v2.1.0
+//Microsoft.Extensions.Localization
+
+builder.Services.AddLocalization(option => option.ResourcesPath = "Resources");
+
+builder.Services.Configure<RequestLocalizationOptions>(o =>
+{
+    List<CultureInfo> cultureInfos = new List<CultureInfo>()
+    {
+        new CultureInfo("en-US"),
+        new CultureInfo("fr-BE")
+    };
+
+    o.SetDefaultCulture("en-US");
+    o.SupportedCultures = cultureInfos;
+    o.SupportedUICultures = cultureInfos;
+});
+
+//await builder.Build().RunAsync();
+
+WebAssemblyHost host = builder.Build();
+
+CultureInfo cultureInfo;
+IJSRuntime js = host.Services.GetRequiredService<IJSRuntime>();
+
+string currentCulture = await js.InvokeAsync<string>("localStorage.getItem", "blazorCulture");
+
+if(currentCulture is not null)
+{
+    cultureInfo = new CultureInfo(currentCulture);
+}
+else
+{
+    cultureInfo= new CultureInfo("en-US");
+    await js.InvokeVoidAsync("localStorage.setItem", "blazorCulture", cultureInfo.Name);
+}
+
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+await host.RunAsync();
